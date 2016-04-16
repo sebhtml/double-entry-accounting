c1c2e4bd4025b7bfbe4fe19e9a6eccf8f41ecd0c
Sébastien Boisvert <seb@boisvert.info>
2016-04-15 23:15:27 -0400

    add virtual transactions
    Signed-off-by: Sébastien Boisvert <seb@boisvert.info>

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import copy
 
 class Transaction:
     def __init__(self, line):
@@ -18,6 +19,8 @@ class Transaction:
         self.modifier = float(tokens[6])
         self.approved = int(tokens[7])
         self.bad = False
+    def getDate(self):
+        return self.date
     def getDestinationAccount(self):
         return self.destinationAccount
     def getSourceAccount(self):
@@ -26,12 +29,29 @@ class Transaction:
         return self.currency
     def getAmount(self):
         return self.amount
+    def getDescription(self):
+        return self.description
     def getModifier(self):
         return self.modifier
     def getApproved(self):
         return self.approved
     def isBad(self):
         return self.bad
+    def makeVirtualSelfTransaction(self):
+        transaction = copy.copy(self)
+        transaction.amount = (1 - self.getModifier()) * self.getAmount()
+        transaction.modifier = 1
+        transaction.destinationAccount = "virt-" + self.getSourceAccount() + "-destination-self"
+        transaction.sourceAccount = "virt-" + self.getSourceAccount() + "-source"
+        return transaction
+    def makeVirtualOtherTransaction(self):
+        transaction = copy.copy(self)
+        transaction.amount = self.getModifier() * self.getAmount()
+        transaction.modifier = 1
+        transaction.destinationAccount = "virt-" + self.getSourceAccount() + "-destination-other"
+        transaction.sourceAccount = "virt-" + self.getSourceAccount() + "-source"
+        return transaction
+
 
 class TransactionFactory:
     def __init__(self):
@@ -58,20 +78,20 @@ class Account:
             print("Error: accounts are the same")
             print(transaction)
             return
-        self.transactions.append(transaction)
-
         amount = transaction.getAmount()
-
         if transaction.getDestinationAccount() == self.getName():
             self.balance += amount
         elif transaction.getSourceAccount() == self.getName():
             self.balance -= amount
+        self.transactions.append(transaction)
     def getCurrency(self):
         return self.currency
     def getBalance(self):
         return self.balance
     def getName(self):
         return self.name
+    def getTransactions(self):
+        return self.transactions
 
 class AccountFactory:
     def __init__(self):
@@ -85,7 +105,31 @@ class AccountFactory:
     def getAccount(self, name, currency):
         return self.makeAccount(name, currency)
     def getAccounts(self):
-        return self.accounts.values()
+        accounts = []
+        names = self.accounts.keys()
+        names.sort()
+
+        for i in names:
+            accounts.append(self.accounts[i])
+        return accounts
+
+def processTransaction(transaction, makeVirtualTransaction, accountFactory):
+    destinationAccount = transaction.getDestinationAccount()
+    sourceAccount = transaction.getSourceAccount()
+
+    currency = transaction.getCurrency()
+    if destinationAccount != sourceAccount:
+        accountFactory.getAccount(destinationAccount, currency).addTransaction(transaction)
+        accountFactory.getAccount(sourceAccount, currency).addTransaction(transaction)
+
+        if not makeVirtualTransaction:
+            return
+
+        virtualSelfTransaction = transaction.makeVirtualSelfTransaction()
+        processTransaction(virtualSelfTransaction, False, accountFactory)
+
+        virtualOtherTransaction = transaction.makeVirtualOtherTransaction()
+        processTransaction(virtualOtherTransaction, False, accountFactory)
 
 
 def main(arguments):
@@ -103,21 +147,39 @@ def main(arguments):
                         transaction = transactionFactory.makeTransaction(line)
                         if transaction.isBad():
                             continue
-                        destinationAccount = transaction.getDestinationAccount()
-                        sourceAccount = transaction.getSourceAccount()
 
-                        currency = transaction.getCurrency()
-                        if destinationAccount != sourceAccount:
-                            accountFactory.getAccount(destinationAccount, currency).addTransaction(transaction)
-                            accountFactory.getAccount(sourceAccount, currency).addTransaction(transaction)
+                        processTransaction(transaction, True, accountFactory)
+
+    print("Transactions")
+    print("")
+    for account in accountFactory.getAccounts():
+        name = account.getName()
+        currency = account.getCurrency()
+        print("account: " + name + "        currency: " + currency)
+
+        transactionString = "  %-15s %-20s %-30s %-30s %10s %10s"
+        print(transactionString % ("Date", "Description", "DestinationAccount", "SourceAccount", "Amount", "Currency"))
+
+        for transaction in account.getTransactions():
+            date = transaction.getDate()
+            description = transaction.getDescription()
+            destinationAccount = transaction.getDestinationAccount()
+            sourceAccount = transaction.getSourceAccount()
+            amount = transaction.getAmount()
+
+            print("  %-15s %-20s %-30s %-30s %10.2f %10s" % (date, description, destinationAccount, sourceAccount, amount, currency))
+        print("")
 
 
+    print("Balances")
+    #print("")
+    print("%-30s %10s %10s" % ("Account", "Amount", "Currency"))
     for account in accountFactory.getAccounts():
         name = account.getName()
         balance = account.getBalance()
         currency = account.getCurrency()
 
-        print("%15s %10.2f %5s" % (name, balance, currency))
+        print("%-30s %10.2f %10s" % (name, balance, currency))
 
 
 main(sys.argv)
